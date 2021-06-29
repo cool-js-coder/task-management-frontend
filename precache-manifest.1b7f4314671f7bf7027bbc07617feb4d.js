2af67b4ff00bb1c1325b119a38d0bd9d352f284c
cool-js-coder <cool-js-coder@users.noreply.github.com>
2021-06-29 06:23:25 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.1b7f4314671f7bf7027bbc07617feb4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6eaeac5500ce1e8e254a3afd8ccf56",
+    "url": "https://cool-js-coder.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "09b97df52af95ada2914",
+    "url": "https://cool-js-coder.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "95e7cd3353126086832d",
+    "url": "https://cool-js-coder.github.io/task-management-frontend/static/js/2.1a6d437e.chunk.js"
+  },
+  {
+    "revision": "09b97df52af95ada2914",
+    "url": "https://cool-js-coder.github.io/task-management-frontend/static/js/main.e6ccc6a5.chunk.js"
+  },
+  {
+    "revision": "7866668c51fdd01b89c3",
+    "url": "https://cool-js-coder.github.io/task-management-frontend/static/js/runtime~main.c35c74dc.js"
+  }
+]);
